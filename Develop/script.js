579e7e26bfa11980a97123da77ef46071812f415
Heather Stevens <heatherveva@gmail.com>
2022-01-06 15:23:44 -0600

    hw3 progress

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,159 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 // 1b. create array of letters
-let lcLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"];
-let ucLetters = ["A", "B", "C", "D"];
+let possibleLetters = [];
+let lcLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let ucLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let characters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+let specialCharacters = [
+  "\\",
+  ".",
+  "+",
+  "*",
+  "?",
+  "[",
+  "^",
+  "]",
+  "$",
+  "(",
+  ")",
+  "{",
+  "}",
+  "=",
+  "!",
+  "<",
+  ">",
+  "|",
+  ":",
+  "-",
+];
 
 // generatePassword
 // return a final password as a string
 function generatePassword() {
-  let userInput = window.prompt(
-    `Please choose a password length. Enter it below.`
+  // 1. assign a variable to the length of the password
+  let password = [];
+
+  let pwLength = parseInt(
+    prompt(`Please choose a password length. Enter it below.`)
   );
 
   let minLength = 8;
   let maxLength = 128;
-  {
-    if (userInput >= minLength && userInput <= maxLength) {
-      alert("Thank you!");
-      return true;
-    } else {
-      alert(
-        `Please input a number between ${minLength} and ${maxLength} characters to continue.`
-      );
-      return false;
-    }
+
+  if (pwLength >= minLength && pwLength <= maxLength) {
+    alert("Thank you!");
+  } else {
+    alert(
+      `Please input a number between ${minLength} and ${maxLength} characters to continue.`
+    );
   }
+  let hasLcLetters = window.confirm(
+    "Would you like your password to contain lowercase letters?"
+  );
 
-  //choose upper, lower, special, numbers
+  let hasUcLetters = window.confirm(
+    "Would you like your password to contain uppercase letters?"
+  );
 
-  let password = [];
+  let hasNumbers = window.confirm(
+    "Would you like your password to contain numbers?"
+  );
+
+  let hasSpecialCharacters = window.confirm(
+    "Would you like your password to contain symbols?"
+  );
+
   // 1a. create a password with that length
   // 1c. loop through array of letters
-  for (let i = 0; i < pwLength; i++) {
-    // 1d. push each letter array into password array
-    password.push(lcLetters[i]);
+  if (hasLcLetters === true) {
+    possibleLetters = possibleLetters.concat(lcLetters);
+    password.push(generateRandom(lcLetters));
+    pwLength--;
   }
 
-  // 1e. create a random 10 letter password
-  // 2 ask user for length
+  if (hasUcLetters === true) {
+    possibleLetters = possibleLetters.concat(ucLetters);
+    password.push(generateRandom(ucLetters));
+    pwLength--;
+  }
 
+  if (hasNumbers === true) {
+    possibleLetters = possibleLetters.concat(numbers);
+    password.push(generateRandom(numbers));
+    pwLength--;
+  }
+
+  if (hasSpecialCharacters === true) {
+    possibleLetters = possibleLetters.concat(specialCharacters);
+    password.push(generateRandom(specialCharacters));
+    pwLength--;
+  }
+
+  // 1d. push each letter array into password array
+  for (i = 0; i < pwLength; i++) {
+    password.push(generateRandom(possibleLetters));
+  }
   // 1e. convert password array back to string
   return password.join("");
+}
+function generateRandom(characters) {
+  let index = Math.floor(Math.random() * characters.length);
+  return characters[index];
 }
 // Write password to the #password input
 function writePassword() {
